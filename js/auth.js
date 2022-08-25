f0b68c4fd414d0006652d30fcdae624bf354a194
ioyli <veera.yli-kleemola@esedulainen.fi>
2022-08-25 13:35:12 +0300

    Basic login functionality

---
+++ js/auth.js
@@ -0,0 +1,21 @@
+class Auth {
+    // hide body by default
+    constructor() {
+        document.querySelector("body").style.display = "none";
+        const auth = localStorage.getItem("auth");
+        this.validateAuth(auth);
+    }
+
+    validateAuth(auth) {
+        if (auth !== 1) {
+            window.location.replace("/");
+        } else {
+            document.querySelector("body").style.display = "block";
+        }
+    }
+
+    logOut() {
+        localStorage.removeItem("auth");
+        window.location.replace("/");
+    }
+}
---
+++ js/login.js
@@ -0,0 +1,90 @@
+class Login {
+    constructor(form, fields) {
+        this.form = form;
+        this.fields = fields;
+        this.validateOnSubmit();
+    }
+
+    validateOnSubmit() {
+        let self = this;
+
+        // add submit event listener to form
+        this.form.addEventListener("submit", (e) => {
+
+            e.preventDefault();
+            let error = 0;
+
+            // loop through fields and check each for validation
+            self.fields.forEach((field) => {
+                const input = document.querySelector(`#${field}`);
+                if (self.validateFields(input) === false) {
+                    error++;
+                }
+            });
+
+            if (error === 0) {
+                localStorage.setItem("auth", 1);
+                this.form.submit();
+            }
+        })
+    }
+
+    validateFields(field) {
+        // remove any white space, if field is blank, return false
+        if (field.value.trim() === "") {
+            // set status based on field, field label, and error message
+            this.setStatus(
+                field,
+                `${field.previousElementSibling.innerText} cannot be blank.`, "error"
+            );
+            return false;
+
+        } else {
+            // if not blank, check if the field is a password
+            if (field.type === "password") {
+                // if a password, check to see if it meets minimum requirements
+                if (field.value.length < 8) {
+                    // set status based on field, field label, and error message
+                    this.setStatus(
+                        field,
+                        `${field.previousElementSibling.innerText} must be at least 8 characters long.`, "error"
+                    );
+                    return false;
+
+                } else {
+                    // set status based on field, no text, and success message
+                    this.setStatus(field, null, "success");
+                    return true;
+                }
+            } else {
+                // set status based on field, no text, and success message
+                this.setStatus(field, null, "success");
+                return true;
+            }
+        }
+    }
+
+    setStatus(field, message, status) {
+        const errorMessage = field.parentElement.querySelector(".errorMessage");
+
+        // if success, remove error messages and classes
+        if (status === "success") {
+            if (errorMessage) {
+                errorMessage.innerText = "";
+            }
+            field.classList.remove("inputError");
+        }
+
+        if (status === "error") {
+            errorMessage.innerText = message;
+            field.classList.add("inputError");
+        }
+    }
+}
+
+const form = document.getElementById('loginRegister');
+// if form exists, run class
+if (form) {
+    const fields = ["username", "password"];
+    const validator = new Login (form, fields);
+}
